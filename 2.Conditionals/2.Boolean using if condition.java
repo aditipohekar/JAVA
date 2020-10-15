0791ebc41a8412114bd4467d77c630dd1c1c622e
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-10-15 19:57:57 +0530

    Create 2.Boolean using if condition.java

---
+++ 2.Conditionals/2.Boolean using if condition.java
@@ -0,0 +1,11 @@
+public class Main{
+  public static void main(String[]args){
+    int a = 4;
+    int b = 5;
+    if (a == b){
+    System.out.println("Another line Wow!");}
+    else{
+    System.out.println("Double rainbow!");
+  }
+}
+}
