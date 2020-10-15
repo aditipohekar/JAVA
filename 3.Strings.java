08abd02993cee669f122d0d4bc4373275e088517
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-10-15 18:44:05 +0530

    Create 3.Strings.java

---
+++ 3.Strings.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+String s1 = new String("Who let the dogs out?");     
+
+String s2 = "Who who who who!";                 
+String s3 = s1 + s2;
+        System.out.println(s3);
+    }
+}
