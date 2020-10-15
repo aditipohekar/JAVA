d96440e78946915f1fa841e947d9696c359e0516
ADITI POHEKAR <57346892+aditipohekar@users.noreply.github.com>
2020-10-15 20:27:59 +0530

    Delete 3.Strings.java

--- 1.Introduction/3.Strings.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-String s1 = new String("Who let the dogs out?");     
-
-String s2 = "Who who who who!";                 
-String s3 = s1 + s2;
-        System.out.println(s3);
-    }
-}
